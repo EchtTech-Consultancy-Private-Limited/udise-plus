6f8736b9ce26cfd07af47ddf1fb440e1529376ed
suraj.echttech <suraj.chauhan@echttech.com>
2024-02-16 09:57:41 +0530

    changed api endpoint

--- src/redux/thunks/stateThunk.js
+++ src/redux/thunks/stateThunk.js
@@ -4,7 +4,7 @@ import axios  from '../../services/utility';
 const fetchStateData = createAsyncThunk(
   "state/fetchStateData",
   async () => {
-    const response = await axios.get(`states/`);
+    const response = await axios.get(`states/get-all`);
     return response.data;
   }
 );
