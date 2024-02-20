96fa5df3a580d765b16ec7cb13a800916159a532
suraj.echttech <suraj.chauhan@echttech.com>
2024-02-20 17:44:13 +0530

    new-build

--- src/components/Report/InfrastructureReport3013.jsx
+++ src/components/Report/InfrastructureReport3013.jsx
@@ -31,27 +31,6 @@ export default function Infrastructure3013() {
       date: "09-02-2022",
       available: true,
     },
-    {
-      make: "Ford",
-      model: "Mondeo",
-      price: 32000,
-      date: "11-02-2022",
-      available: false,
-    },
-    {
-      make: "Porsche",
-      model: "Boxter",
-      price: 72000,
-      date: "10-02-2022",
-      available: true,
-    },
-    {
-      make: "Mers",
-      model: "Mers",
-      price: 92000,
-      date: "12-02-2022",
-      available: true,
-    },
   ];
 
   const columns = [
@@ -218,7 +197,6 @@ const getDocument = (gridApi) => {
 };
 
  const exportToPDF = () => {
-  console.log(gridApi,' grid api')
   const doc = getDocument(gridApi);
   pdfMake.createPdf(doc).open();
 };
