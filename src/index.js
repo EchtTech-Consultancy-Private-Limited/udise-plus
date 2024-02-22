61457b2dc2819274d4bf684bad6859a1da4ecb04
suraj.echttech <suraj.chauhan@echttech.com>
2024-02-22 16:28:02 +0530

    working lang

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+      <App />
   </Provider>     
 );
 
