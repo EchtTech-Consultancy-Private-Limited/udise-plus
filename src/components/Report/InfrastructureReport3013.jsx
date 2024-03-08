6254d02913cbe7bc2e923675e4d3a6340223b56a
suraj.echttech <suraj.chauhan@echttech.com>
2024-03-08 10:52:54 +0530

    pre-changes

--- src/components/Report/InfrastructureReport3013.jsx
+++ src/components/Report/InfrastructureReport3013.jsx
@@ -585,6 +585,7 @@ const liveTime= `dateTime.toLocaleString('en-US', {day: '2-digit', month: 'short
                       </p>
                     </div>
                   </Tab>
+                  
                   <Tab eventKey="table" title="Table">
                     <div className="col-md-12 d-flex justify-content-end">
                       {/* {hideScrollBtn!==3 && (<button onClick={() => scrollToRight()} className="scroll-right-btn" title="Scroll to Right "><span className="material-icons-round">arrow_right_alt</span> </button>)}
